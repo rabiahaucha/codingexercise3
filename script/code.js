53ddb93409cc5f7afd126000f0bf31e4d7445106
rabia <rabiahaucha9@gmail.com>
2023-05-17 15:40:55 +0200

    exercise3

---
+++ script/code.js
@@ -0,0 +1,31 @@
+   //string//
+let firstName = 'mujahid'
+ console.log(typeof firstName); 
+
+//number//
+  let age = 21
+  console.log( typeof age)
+
+//object//
+ let data = {first: 'Rabia',
+     last:'Haucha'
+};
+  console.log(typeof data);
+
+//undefined//
+ let ra ;
+ console.log(typeof ra );
+ 
+
+//array//
+ let subject = ["Math","Geography","History"];
+console.log(typeof subject);
+
+ //null//
+ let bird = null;
+ console.log(typeof null);
+
+//boolean//
+ let bag = true ;
+ console.log(typeof true)
+
